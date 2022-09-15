1c0b1e22177bc11f2529e6eb38a9fbb076c73550
LStaley <lstaley9185@gmail.com>
2022-09-15 14:38:04 -0400

    Lab 04 example (Finished)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int intOperandA = 23;
+        int intOperandB = 56;
+        int intSum = intOperandA + intOperandB;
+         System.out.println("The Sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+         double doubleOperandA = 56.7;
+         double doubleOperandB = 43.3;
+         double doubleSum = doubleOperandA + doubleOperandB;
+
+         System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
     }
 }
